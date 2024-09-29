ecf97556e5898a76989f5a40b80155bbbc3dc9be
AleksanderJozwiak <jozwiakaleksander@gmail.com>
2024-09-29 11:13:45 +0200

    FEAT: Basic movement

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity = 100f;
+
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 2f * Time.deltaTime;
+
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+    public float rotationSpeed = 250f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2.5f;
+
+    private CharacterController characterController;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGrounded = characterController.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float moveForward = Input.GetAxis("Vertical");
+        float moveRight = Input.GetAxis("Horizontal");
+
+        Vector3 move = (transform.forward * moveForward + transform.right * moveRight).normalized;
+        characterController.Move(move * moveSpeed * Time.deltaTime);
+
+        float rotation = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
+
